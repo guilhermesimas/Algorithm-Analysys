431af208460b0141e56b5cb816f7573c99186e1c
Guilherme Simas <guilherme_abinader@hotmail.com>
2016-10-15 13:02:37 -0300

    pseudocodigo implementado

--- problem1.c
+++ problem1.c
@@ -24,9 +24,46 @@ int main( int argc, char const *argv[] ) {
 		numberInBinary[nBits] = '\0';
 		// printf( "%s\n", numberInBinary );
 		number[i] = strtol( numberInBinary, NULL, BASE );
-		printf( "%d\n", number[i] );
+		// printf( "%d\n", number[i] );
 	}
+
+
 
 	fclose( pFile );
 	return 0;
+}
+
+int degrau_2_frascos ( long int x, int n ) {
+	int raiz_n = ( int )sqrt( n );
+	for ( int i = 0; i < n; i += raiz_n ) {
+		/* code */
+		if ( i >= x ) {
+			for ( int j = i - raiz_n; j < i; j++ ) {
+				/* code */
+				if ( j == x ) {
+					return j;
+				}
+			}
+		}
+	}
+}
+
+degrau_k_frascos( long int x, int n, int k ) {
+	int raiz_kesima = ( int )pow( n, 1.0f / k );
+	int inicio = 0;
+	int fim = n;
+	int incremento = ( int ) pow( raiz_kesima, k - 1 );
+	for ( int i = 0; i < k; i++ ) {
+		for ( int j = inicio; j < fim; j += incremento ) {
+			if ( j >= x ) {
+				if ( incremento == 1 ) {
+					return j;
+				}
+				inicio = j - incremento;
+				fim = j;
+				incremento = incremento / raiz_kesima;
+				break;
+			}
+		}
+	}
 }
